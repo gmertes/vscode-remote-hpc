31c7f5d4e98b0b015208d94d01e6e36b6244a5ac
Gert Mertes <hey@gertmertes.be>
2024-04-29 16:17:51 +0000

    Check job state
    This prevents starting a new job if there is already one pending

--- vscode-remote.sh
+++ vscode-remote.sh
@@ -13,17 +13,21 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 START=$(date +%s)
 
 query_job(){
-    list=($(squeue --me --states=R -h -O JobId:" ",Name:" ",NodeList:" " | grep $JOB_NAME))
+    # only list states that can resolve to a running job
+    list=($(squeue --me --states=R,PD,S,CF,RF,RH,RQ -h -O JobId:" ",Name:" ",State:" ",NodeList:" " | grep $JOB_NAME))
     if [ ! ${#list[@]} -eq 0 ]; then
+        echo ${list[@]}
         JOB_ID=${list[0]}
         JOB_FULLNAME=${list[1]}
-        JOB_NODE=${list[2]}
+        JOB_STATE=${list[2]}
+        JOB_NODE=${list[3]}
 
         split=(${JOB_FULLNAME//%/ })
         JOB_PORT=${split[1]}
     else
         JOB_ID=""
         JOB_FULLNAME=""
+        JOB_STATE=""
         JOB_NODE=""
         JOB_PORT=""
     fi
@@ -49,22 +53,22 @@ if [ ! -z "$1" ] && [ $1 == "cancel" ]; then
 fi
 
 if [ ! -z "$1" ] && [ $1 == "list" ]; then
-    echo "$(squeue --me --states=R -O JobId,Partition,Name,UserName,State,TimeUsed,TimeLimit,NodeList | grep -E "JOBID|$JOB_NAME")"
+    echo "$(squeue --me -O JobId,Partition,Name,State,TimeUsed,TimeLimit,NodeList | grep -E "JOBID|$JOB_NAME")"
     exit 0
 fi
 
 query_job
 
-if [ -z "${JOB_NODE}" ]; then
+if [ -z "${JOB_STATE}" ]; then
     PORT=$(shuf -i 2000-65000 -n 1)
     /usr/bin/sbatch -J $JOB_NAME%$PORT $SBATCH_PARAM -o none $SCRIPT_DIR/job.sh $PORT
-
-    while [ -z "${JOB_NODE}" ]; do
-        timeout
-        sleep 5
-        query_job
-    done
 fi
+
+while [ ! "$JOB_STATE" == "RUNNING" ]; do
+    timeout
+    sleep 5
+    query_job
+done
 
 echo "Connecting to $JOB_NODE"
 
