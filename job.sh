ddfa2f0f048c8cf6fa8cba73be2785cc78eff7d5
Gert Mertes <hey@gertmertes.be>
2024-05-02 15:28:29 +0000

    Automatically generate sshd hostkey

--- job.sh
+++ job.sh
@@ -2,4 +2,12 @@
 
 #SBATCH -o none
 
-/usr/sbin/sshd -D -p $1 -f /dev/null -h ${HOME}/.ssh/id_ed25519
+if [ ! -d "${HOME:-~}.ssh" ]; then
+    mkdir -p ${HOME:-~}/.ssh
+fi
+
+if [ ! -f "${HOME:-~}/.ssh/vscode-remote-hostkey" ]; then
+    ssh-keygen -t ed25519 -f ${HOME:-~}/.ssh/vscode-remote-hostkey -N ""
+fi
+
+/usr/sbin/sshd -D -p $1 -f /dev/null -h ${HOME:-~}/.ssh/vscode-remote-hostkey
