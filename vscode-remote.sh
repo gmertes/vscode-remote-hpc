6abb6c8b8d3c5b814ceeef6fd487009be5b6a9f4
Gert Mertes <hey@gertmertes.be>
2024-05-03 13:39:32 +0000

    Add help

--- vscode-remote.sh
+++ vscode-remote.sh
@@ -1,20 +1,45 @@
 #!/bin/bash
 
-JOB_NAME=vscode-remote
+# Set your Slurm parameters for GPU and CPU jobs here
+SBATCH_PARAM_CPU="-q ni -t 12:00:00 --mem=32G -c 8"
+SBATCH_PARAM_GPU="-q ng --gpus=1 -t 04:00:00 --mem=32G -c 8"
+
+# The time you expect a job to start in (seconds)
+# If a job doesn't start within this time, the script will exit and cancel the pending job
 TIMEOUT=300
 
-if [ ! -z "$1" ] && [ $1 == "gpu" ]; then
-    # GPU
-    SBATCH_PARAM="-q ng --gpus=1 -t 04:00:00 --mem=32G -c 8"
-else
-    # CPU
-    SBATCH_PARAM="-q ni -t 12:00:00 --mem=32G -c 8"
-fi
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-START=$(date +%s)
+####################
+# don't edit below this line
+####################
 
-query_slurm(){
+function usage ()
+{
+    echo "Usage :  $0 [command]
+
+    General commands:
+    list      List running vscode-remote job
+    cancel    Cancels running vscode-remote job
+    ssh       SSH into the node of a running job
+    help      Display this message
+
+    Job commands (see usage below):
+    cpu       Connect to a CPU node
+    gpu       Connect to a GPU node
+
+    You should not manually call the script with 'cpu' or 'gpu' commands.
+    They should be used in the ProxyCommand in your ~/.ssh/config file, for example:
+        Host vscode-remote-cpu
+            User USERNAME
+            IdentityFile ~/.ssh/vscode-remote
+            ProxyCommand ssh HPC-LOGIN \"bash --login -c 'vscode-remote cpu'\"
+            StrictHostKeyChecking no  
+
+    You can only have one job type at a time (cpu or gpu). If you want to switch job types, you have to first run 'cancel'.
+    "
+} 
+
+function query_slurm () {
     # only list states that can result in a running job
     list=($(squeue --me --states=R,PD,S,CF,RF,RH,RQ -h -O JobId:" ",Name:" ",State:" ",NodeList:" " | grep $JOB_NAME))
 
@@ -37,7 +62,7 @@ query_slurm(){
     fi
 }
 
-timeout() {
+function timeout () {
     if (( $(date +%s)-START > TIMEOUT )); then 
         >&2 echo "Timeout, exiting..."
         if [ ! -z "${JOB_SUBMIT_ID}" ]; then
@@ -48,7 +73,7 @@ timeout() {
     fi
 }
 
-if [ ! -z "$1" ] && [ $1 == "cancel" ]; then
+function cancel () {
     query_slurm > /dev/null 2>&1
     while [ ! -z "${JOB_ID}" ]; do
         echo "Cancelling running job $JOB_ID on $JOB_NODE"
@@ -57,15 +82,14 @@ if [ ! -z "$1" ] && [ $1 == "cancel" ]; then
         sleep 2
         query_slurm > /dev/null 2>&1
     done
-    exit 0
-fi
+}
 
-if [ ! -z "$1" ] && [ $1 == "list" ]; then
-    echo "$(squeue --me -O JobId,Partition,Name,State,TimeUsed,TimeLimit,NodeList | grep -E "JOBID|$JOB_NAME")"
-    exit 0
-fi
+function list () {
+    width=$((${#JOB_NAME} + 11))
+    echo "$(squeue --me -O JobId,Partition,Name:$width,State,TimeUsed,TimeLimit,NodeList | grep -E "JOBID|$JOB_NAME")"
+}
 
-if [ ! -z "$1" ] && [ $1 == "ssh" ]; then
+function ssh_connect () {
     query_slurm
     if [ -z "${JOB_NODE}" ]; then
         echo "No running job found"
@@ -73,29 +97,50 @@ if [ ! -z "$1" ] && [ $1 == "ssh" ]; then
     fi
     echo "Connecting to $JOB_NODE via SSH"
     ssh $JOB_NODE
+}
+
+function connect () {
+    query_slurm
+
+    if [ -z "${JOB_STATE}" ]; then
+        PORT=$(shuf -i 2000-65000 -n 1)
+        list=($(/usr/bin/sbatch -J $JOB_NAME%$PORT $SBATCH_PARAM $SCRIPT_DIR/job.sh $PORT))
+        JOB_SUBMIT_ID=${list[3]}
+        >&2 echo "Submitted new $JOB_NAME job (id: $JOB_SUBMIT_ID)"
+    fi
+
+    while [ ! "$JOB_STATE" == "RUNNING" ]; do
+        timeout
+        sleep 5
+        query_slurm
+    done
+
+    >&2 echo "Connecting to $JOB_NODE"
+
+    while ! nc -z $JOB_NODE $JOB_PORT; do 
+        timeout
+        sleep 1 
+    done
+
+    nc $JOB_NODE $JOB_PORT
+}
+
+if [ ! -z "$1" ]; then
+    JOB_NAME=vscode-remote
+    SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+    START=$(date +%s)
+    case $1 in
+        list)   list ;;
+        cancel) cancel ;;
+        ssh)    ssh_connect ;;
+        cpu)    JOB_NAME=$JOB_NAME-cpu; SBATCH_PARAM=$SBATCH_PARAM_CPU; connect ;;
+        gpu)    JOB_NAME=$JOB_NAME-gpu; SBATCH_PARAM=$SBATCH_PARAM_GPU; connect ;;
+        help)   usage ;;
+        *)  echo -e "Command '$1' does not exist" >&2
+            usage; exit 1 ;;
+    esac  
+    exit 0
+else
+    usage
     exit 0
 fi
-
-query_slurm
-
-if [ -z "${JOB_STATE}" ]; then
-    PORT=$(shuf -i 2000-65000 -n 1)
-    list=($(/usr/bin/sbatch -J $JOB_NAME%$PORT $SBATCH_PARAM $SCRIPT_DIR/job.sh $PORT))
-    JOB_SUBMIT_ID=${list[3]}
-    >&2 echo "Submitted new $JOB_NAME job (id: $JOB_SUBMIT_ID)"
-fi
-
-while [ ! "$JOB_STATE" == "RUNNING" ]; do
-    timeout
-    sleep 5
-    query_slurm
-done
-
->&2 echo "Connecting to $JOB_NODE"
-
-while ! nc -z $JOB_NODE $JOB_PORT; do 
-    timeout
-    sleep 1 
-done
-
-nc $JOB_NODE $JOB_PORT
